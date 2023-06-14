ef7cf63af4964b1ff36aeb76359305c90e5be378
Yūri Katsuki <yuri@katsuki.moe>
2023-06-14 16:13:06 -0400

    [NEW] initial project stage

---
+++ src/lib.rs
@@ -0,0 +1,125 @@
+use proc_macro::{Group, Ident, TokenStream, TokenTree};
+
+fn replace_ident(ident: Ident) -> Option<TokenTree> {
+    let ident_str = ident.to_string();
+
+    let new_str = match ident_str.as_str() {
+        "Xato" => "Err",
+        "Yaxshi" => "Ok",
+        "Chaine" => "String",
+        "Lug`at" => "HashMap",
+        "Odatiy" => "Default",
+        "Xatolik" => "Error",
+        "Tanlov" => "Option",
+        "Quelque" => "Some",
+        "Rien" => "None",
+        "Natija" => "Result",
+        "Shu" => "Self",
+        "yozq" => "println",
+        "to`xta" => "break",
+        "asinxron" => "async",
+        "kut" => "await",
+        "takror" => "loop",
+        "ko`chir" => "move",
+        "quti" => "crate",
+        "code_inaccessible" => "unreachable_code",
+        "comme" => "as",
+        "constant" => "const",
+        "hislat" => "trait",
+        "dangereux" => "unsafe",
+        "de" => "in",
+        "depuis" => "from",
+        "dynamique" => "dyn",
+        "déballer" => "unwrap",
+        "défaut" => "default",
+        "en_réf" => "as_ref",
+        "es" => "io",
+        "externe" => "extern",
+        "yo`q" => "false",
+        "fonction" => "fn",
+        "génial" => "super",
+        "insérer" => "insert",
+        "lire" => "get",
+        "légal" => "allow",
+        "merde" | "calisse" | "oups" => "panic",
+        "module" => "mod",
+        "o`zgaruvchan" => "mut",
+        "yangi" => "new",
+        "où" => "where",
+        "pour" => "for",
+        "prendre_ou_insérer_avec" => "get_or_insert_with",
+        "asosiy" => "main",
+        "ochiq" => "pub",
+        "que" => None?,
+        "qaytar" => "return",
+        "réalisation" => "impl",
+        "réf" => "ref",
+        "selon" => "match",
+        "agar" => "if",
+        "unda" => "else",
+        "shu" => "self",
+        "joy" => "let",
+        "statik" => "static",
+        "structure" => "struct",
+        "suppose" => "expect",
+        "tant" => "while",
+        "utilisons" => "use",
+        "vers" => "into",
+        "ha" => "true",
+        "tur" => "enum",
+        "Guruh" => "Group",
+        "Identifiant" => "Ident",
+        "FluxDeJetons" => "TokenStream",
+        "ArbreDeJetons" => "TokenTree",
+        "vers_chaîne" => "to_string",
+        "en_tant_que_chaîne" => "as_str",
+        "portée" => "span",
+        "Tableau" => "Vec",
+        "flux" => "stream",
+        "pousser" => "push",
+        "étendre" => "extend",
+        "délimiteur" => "delimiter",
+        "Ponctuation" => "Punct",
+        "Litéral" => "Literal",
+        "macro_procédurale" => "proc_macro",
+        _ => &ident_str,
+    };
+
+    let new_ident = Ident::new(new_str, ident.span());
+    Some(TokenTree::Ident(new_ident))
+}
+
+fn replace_tree(tok: TokenTree, out: &mut Vec<TokenTree>) {
+    match tok {
+        TokenTree::Group(group) => {
+            let mut group_elem = Vec::new();
+            replace_stream(group.stream(), &mut group_elem);
+            let mut new_stream = TokenStream::new();
+            new_stream.extend(group_elem);
+            out.push(TokenTree::Group(Group::new(group.delimiter(), new_stream)));
+        }
+        TokenTree::Ident(ident) => {
+            if let Some(ident) = replace_ident(ident) {
+                out.push(ident);
+            }
+        }
+        TokenTree::Punct(..) | TokenTree::Literal(..) => {
+            out.push(tok);
+        }
+    }
+}
+
+fn replace_stream(ts: TokenStream, out: &mut Vec<TokenTree>) {
+    for tok in ts {
+        replace_tree(tok, out)
+    }
+}
+
+#[proc_macro]
+pub fn zang(item: TokenStream) -> TokenStream {
+    let mut returned = Vec::new();
+    replace_stream(item, &mut returned);
+    let mut out = TokenStream::new();
+    out.extend(returned);
+    out
+}
